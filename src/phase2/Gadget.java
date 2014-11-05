42353b693229cff145183fa31b95c1df60cc1843
Lisa Ho <lisayz@mit.edu>
2014-11-05 13:40:44 -0500

    Added fields to Gadget

--- src/phase2/Gadget.java
+++ src/phase2/Gadget.java
@@ -1,8 +1,25 @@
 package phase2;
 
+import Gadget;
+import GameObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import physics.Angle;
+import physics.Circle;
 import physics.Geometry.DoublePair;
 
 public abstract class Gadget {
+    
+    private int positionWidth;
+    private int positionHeight;
+    private double reflectionCoef;
+    private GridPoint location; // the (x,y) coordinates of the top left bounding box of the gadget
+    
+    public Gadget(GridPoint location) {
+        this.location = location;
+    }
 
     /**
      * @return DoublePair representing the (width,height) of the Gadget on the Board
@@ -13,6 +30,16 @@ public abstract class Gadget {
      * @return DoublePair representing the (width,height) of the Gadget
      */
     public DoublePair getSize;
+    
+    /**
+     * @return the gadget's coefficient of reflection
+     */
+    public double coefficientOfReflection;
+    
+    /**
+     * @return the x-coordinate of the gadget's origin
+     */
+    
 
     /**
      * @return list of characters in the form of a String
@@ -22,10 +49,6 @@ public abstract class Gadget {
      */
     public String toString;
 
-    /**
-     * @return DoublePair representing (x,y) coordinates of the Gadget on the board
-     */
-    public DoublePair getBoardRepPosition;
     
     /**
      * Triggers any actions on other Gadgets that it may need to
