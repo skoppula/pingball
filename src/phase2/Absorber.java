454b9d8b4a20a5de1235b186860519ea24e9f0d0
Yonadav Shavit <yoyoyonadav@gmail.com>
2014-11-06 23:31:47 -0500

    Absorber collides now and stuff

--- src/phase2/Absorber.java
+++ src/phase2/Absorber.java
@@ -1,17 +1,13 @@
 package phase2;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 import phase2.Util.InvalidInvariantException;
 import phase2.physicsComponents.PhysicsComponent;
 import physics.Circle;
-import physics.Geometry;
 import physics.LineSegment;
 import physics.Vect;
-import physics.Geometry.DoublePair;
 import phase2.physicsComponents.StaticCircle;
 import phase2.physicsComponents.StaticLine;
 
@@ -91,7 +87,21 @@ public class Absorber extends Gadget {
             this.loadedBall.inAbsorber = true;
             this.loadedBall.setVelocity(new Vect(0.0, 0.0));
             setLoaded(true);
-        } //NEIGHTER BALL NOR ABSORBER IS AFFECTED IF ABSORBER ALREADY LOADED (PASS THROUGH FUNCTIONALITY) 
+        }
+        else{
+        	PhysicsComponent gadgetPartToCollideWith = this.physicsComponentList.get(0);
+            double minTimeUntilCollision = Double.MAX_VALUE;
+            for(PhysicsComponent gadgetPart: physicsComponentList){
+                double timeUntilCollisionPart = gadgetPart.timeUntilCollision(ball.getBallCircle(), ball.getVelocity());
+                if (timeUntilCollisionPart < minTimeUntilCollision){ 
+                    minTimeUntilCollision = timeUntilCollisionPart;
+                    gadgetPartToCollideWith = gadgetPart;
+                }
+            }
+            Vect newVelocity = gadgetPartToCollideWith.reflect(ball.getBallCircle(), ball.getVelocity(), ball.getCoefficentOfReflection()); 
+            ball.setVelocity(newVelocity);
+        }
+        trigger();
     }
 
     /**
