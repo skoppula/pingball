2a79f8027088c385f7ed4209b4c82073ca9136a8
Yonadav Shavit <yoyoyonadav@gmail.com>
2014-11-08 14:28:16 -0500

    Added the phase2.messaging package, which provides support for basic message passing between the server and clients, as well as different parts of the server itself. Each class in this package implements Message, an abstract class. Still need to complete a number of the methods, look for TODOs in code.

---
+++ src/phase2/messaging/BallMessage.java
@@ -0,0 +1,45 @@
+package phase2.messaging;
+
+import phase2.Ball;
+
+/**
+ * A class used to transmit
+ * information about a ball, and the board wall that
+ * is relevant to it. For example, if a board is sending a ball
+ * which should be teleporting to a server, the boardwall is the wall
+ * it originated from.
+ *
+ */
+public class BallMessage extends Message {
+	
+
+	private final Ball ball;
+	private final BoardWallPair boardWall;
+	
+	/**
+	 * Creates a message intended to transmit a ball and
+	 * a board's wall relevant to its action.
+	 * @param ballName the name of the ball being sent
+	 * @param boardWall the 
+	 */
+	public BallMessage(Ball ball, BoardWallPair boardWall){
+		this.ball = ball;
+		this.boardWall = boardWall;
+		this.messageType = MessageType.BALL;
+	}
+	
+	/**
+	 * @return the ball passed in this message
+	 */
+	public Ball getBall(){
+		return ball;
+	}
+	
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/phase2/messaging/ServerWallConnectMessage.java
@@ -0,0 +1,60 @@
+package phase2.messaging;
+
+/**
+ * A class used to transmit information to a server
+ * about connecting two walls
+ *
+ */
+public class ServerWallConnectMessage extends Message {
+	
+	public enum ConnectionType{
+		HORIZONTAL,
+		VERTICAL
+	}
+
+	private final String boardName1;
+	private final ConnectionType connectionType;
+	private final String boardName2;
+	
+	/**
+	 * Create a message denoting a connection between the board named 
+	 * boardName1 and the board named boardName2, between the walls denoted by connectionType.
+	 * Equivalent to the terminal message "connectionType boardName1 boardName2".
+	 * @param boardName1 must be a board that the server is aware of, which does not already have a connection
+	 * of this type
+	 * @param boardName2 must be a board that the server is aware of, which does not already have a connection
+	 * of this type
+	 * @param connectionType
+	 */
+	public ServerWallConnectMessage(String boardName1, String boardName2, ConnectionType connectionType){
+		this.boardName1 = boardName1;
+		this.boardName2 = boardName2;
+		this.connectionType = connectionType;
+		this.messageType = MessageType.SERVERWALLCONNECT;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	public String getBoardName1() {
+		return boardName1;
+	}
+
+
+
+	public String getBoardName2() {
+		return boardName2;
+	}
+
+
+
+	public ConnectionType getConnectionType() {
+		return connectionType;
+	}
+
+}
