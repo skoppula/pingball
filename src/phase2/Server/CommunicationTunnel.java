fefedfee9d823ed4df11ab0747500eb63f1fff11
Yonadav Shavit <yoyoyonadav@gmail.com>
2014-11-13 01:12:59 -0500

    da

--- src/phase2/Server/CommunicationTunnel.java
+++ src/phase2/Server/CommunicationTunnel.java
@@ -65,6 +65,7 @@ public class CommunicationTunnel implements Runnable {
             Thread oh = new Thread(new OutputHandler(out, tunnelOutQ));
             oh.start();
             
+            
         } catch (IOException e) {
         	e.printStackTrace();
         } 
