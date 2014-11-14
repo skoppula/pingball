be14966264094a910e58bde861c7ba08f99fd012
Yonadav Shavit <yoyoyonadav@gmail.com>
2014-11-14 02:28:16 -0500

    minor change

--- src/phase2/Server/QueueProcessor.java
+++ src/phase2/Server/QueueProcessor.java
@@ -166,7 +166,7 @@ public class QueueProcessor implements Runnable {
     		if(wallConnectionMap.containsKey(bwp)){
     			BoardWallPair otherBWP = wallConnectionMap.get(bwp);
     			CommunicationTunnel otherTunnel = nameToBoardTunnelMap.get(otherBWP.board());
-    			otherTunnel.addToOutQ(new ClientWallChangeMessage(otherBWP, false));
+    			otherTunnel.addToOutQ(new ClientWallChangeMessage(bwp, false));
     			wallConnectionMap.remove(bwp);
     		}
     	}
